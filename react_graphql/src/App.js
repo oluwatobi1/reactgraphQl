538b1cb4fd0634fa68aaa7f1771b4b4d78b014b9
oluwatobi1 <akintunlesetobi@gmail.com>
2022-03-02 10:59:10 +0100

    feat;set up router navs

--- react_graphql/src/App.js
+++ react_graphql/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import CharactersLists from './views/CharactersLists';
 
+import { Route, Routes } from "react-router"
+import CharacterDetails from './views/CharacterDetails';
+
 function App() {
   return (
     <div className="App">
-      <CharactersLists/>
+      <Routes>
+        <Route exact path="/" element={<CharactersLists/>} />
+        <Route exact path="/:id" element={<CharacterDetails/>}/>
+      </Routes>
     </div>
   );
 }
