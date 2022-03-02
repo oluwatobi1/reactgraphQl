cf5d1fff4badf8cead0a171c41da80bf1b341425
oluwatobi1 <akintunlesetobi@gmail.com>
2022-03-02 12:50:31 +0100

    feat: mutation demo implementation

---
+++ react_graphql/src/views/Mutations.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { gql, useMutation } from '@apollo/client';
+
+// This is a demo of how mutations work
+// graphql endpoint
+// https://graphql-compose.herokuapp.com/user
+const CREATE_USER =  gql`
+mutation createUserMutation( $name:String!, $age:Int!){
+  userCreate(record:{
+    name:$name
+    age: $age
+  })
+  {
+    record{
+      _id
+      name
+      age
+    }
+  }
+}
+`
+
+export default function Mutations() {
+    const [createUser, {loading, data}] = useMutation(CREATE_USER, { variables:{
+        // inject params dynamically here
+        name:"Oluwatobi",
+        age:23
+    }})
+  return (
+    <div>Mutations Page 
+        <button onClick={()=>createUser}> Create user</button>
+    </div>
+  )
+}
