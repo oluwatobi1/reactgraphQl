3674e7c1d80a5accfa88b1a5f56e28e66f0d0532
oluwatobi1 <akintunlesetobi@gmail.com>
2023-01-01 19:49:39 +0100

    feat: added test for button init state and click

--- Testing React with Jest and React Testing Library (RTL)/color-button/src/App.js
+++ Testing React with Jest and React Testing Library (RTL)/color-button/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [toggleButton, setToggleButton] = useState(true)
+    const handleClick = ()=>{
+        setToggleButton(prevState => !prevState)
+    }
     return (
         <div className="App">
-            <button style={{backgroundColor: "red"}}>
-                Change to Blue
+            <button style={{backgroundColor: toggleButton? "red":"blue"}} onClick={handleClick}>
+                {toggleButton? "Change to Blue": "Change to Red" }
             </button>
 
         </div>
--- Testing React with Jest and React Testing Library (RTL)/color-button/src/App.test.js
+++ Testing React with Jest and React Testing Library (RTL)/color-button/src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import {fireEvent, logRoles, render, screen} from '@testing-library/react';
 import App from './App';
 
-test('button has initial color', () => {
+test('button has initial color and updates when clicked', () => {
   render(<App />);
+
   const colorButton = screen.getByRole("button", {name: "Change to Blue"})
   expect(colorButton).toHaveStyle({backgroundColor: "red"})
+
+  fireEvent.click(colorButton)
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue"});
+  expect(colorButton).toHaveTextContent("Change to Red");
 });
