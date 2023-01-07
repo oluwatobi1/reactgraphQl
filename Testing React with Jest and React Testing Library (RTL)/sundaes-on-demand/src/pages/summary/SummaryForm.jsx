a56660c05848cfb2bee31e1789a23f563f8c50cf
oluwatobi1 <akintunlesetobi@gmail.com>
2023-01-07 23:14:07 +0100

    test: fix wrong test

--- Testing React with Jest and React Testing Library (RTL)/sundaes-on-demand/src/pages/summary/SummaryForm.jsx
+++ Testing React with Jest and React Testing Library (RTL)/sundaes-on-demand/src/pages/summary/SummaryForm.jsx
@@ -1,18 +1,46 @@
 import React, {useState} from 'react';
+import {Form, Button} from "react-bootstrap";
 
 function SummaryForm(props) {
     const [canSubmit, setCanSubmit] = useState(false);
+    const CheckBoxLabel = (
+        <span>
+            i agree to
+            <span style={{color:"blue"}}>
+            Terms and Conditions
+        </span>
+        </span>
+    )
     return (
-        <div>
-            <input type="checkbox" id="t_and_c" defaultChecked={canSubmit} onChange={(e)=>{
-                setCanSubmit(e.target.checked);
-            }}/>
-            <label htmlFor="t_and_c" >
-                Terms and Conditions
-            </label>
+        <Form>
+            <Form.Group controlId="terms_and_conditions">
+                <Form.Check
+                    type="checkbox"
+                    checked={canSubmit}
+                    onChange={(e) => {
+                        setCanSubmit(e.target.checked);
+                    }}
+                    label={CheckBoxLabel}
+                >
 
-            <button disabled={canSubmit}> Submit</button>
-        </div>
+                </Form.Check>
+            </Form.Group>
+            <Button type="submit" variant="primary" disabled={!canSubmit}>
+                Confirm order
+            </Button>
+
+            {/*<div>*/}
+
+            {/*    <input type="checkbox" id="t_and_c" defaultChecked={canSubmit} onChange={(e) => {*/}
+            {/*        setCanSubmit(e.target.checked);*/}
+            {/*    }}/>*/}
+            {/*    <label htmlFor="t_and_c">*/}
+            {/*        Terms and Conditions*/}
+            {/*    </label>*/}
+            {/*    <button disabled={canSubmit}>Confirm Order</button>*/}
+            {/*</div>*/}
+        </Form>
+
     );
 }
 
--- Testing React with Jest and React Testing Library (RTL)/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ Testing React with Jest and React Testing Library (RTL)/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -4,15 +4,18 @@ import SummaryForm from "../SummaryForm";
 
 test("check box initial state is unchecked", ()=>{
     render(<SummaryForm/>);
-    const tcCheckBox = screen.getByRole("checkbox", {name:"Terms and Conditions"})
+    const tcCheckBox = screen.getByRole("checkbox", {name: /terms and conditions/i })
+    const submitButton = screen.getByRole("button", {name: /confirm order/i});
     expect(tcCheckBox).not.toBeChecked()
+    expect(submitButton).toBeDisabled()
+
 })
 test("check box can disable and enable submit btn", ()=>{
     render(<SummaryForm/>);
-    const tcCheckBox = screen.getByRole("checkbox", {name: "Terms and Conditions"});
-    const submitButton = screen.getByRole("button", {name: "Submit"});
-    fireEvent.click(tcCheckBox);
-    expect(submitButton).toBeDisabled();
+    const tcCheckBox = screen.getByRole("checkbox", {name: /terms and conditions/i});
+    const submitButton = screen.getByRole("button", {name: /confirm order/i});
     fireEvent.click(tcCheckBox);
     expect(submitButton).toBeEnabled();
+    fireEvent.click(tcCheckBox);
+    expect(submitButton).toBeDisabled();
 })
