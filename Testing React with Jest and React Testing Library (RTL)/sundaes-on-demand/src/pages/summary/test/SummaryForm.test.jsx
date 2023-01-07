b844574100d6d8e4dad0368264ebe385dd30d43d
oluwatobi1 <akintunlesetobi@gmail.com>
2023-01-07 23:50:28 +0100

    test: added test for term and condtions pop over

--- Testing React with Jest and React Testing Library (RTL)/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ Testing React with Jest and React Testing Library (RTL)/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -21,3 +21,20 @@ test("check box can disable and enable submit btn", async ()=>{
     await user.click(tcCheckBox);
     expect(submitButton).toBeDisabled();
 })
+
+test("Pop over responds to hover", async ()=>{
+    render(<SummaryForm/>);
+    // no pop over on page load
+    const noPopOver = screen.queryByText(/no ice cream will actually be delivered/i);
+    expect(noPopOver).not.toBeInTheDocument()
+    // pop over appears on hover
+    const user = userEvent.setup();
+    const termAndCond = screen.getByText(/terms and conditions/i)
+    await user.hover(termAndCond);
+    const popover = screen.getByText(/no ice cream will actually be delivered/i);
+    expect(popover).toBeInTheDocument()
+
+    // pop over disappear on mouse leave
+    await user.unhover(termAndCond);
+    expect(popover).not.toBeInTheDocument()
+})
