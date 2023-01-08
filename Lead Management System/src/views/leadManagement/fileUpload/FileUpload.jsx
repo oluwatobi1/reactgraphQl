c06b1c62285d3838d7b4f58626cf7f6c6a36a423
oluwatobi1 <akintunlesetobi@gmail.com>
2023-01-08 17:24:33 +0100

    feat:implemented form ui

--- Lead Management System/src/views/leadManagement/fileUpload/FileUpload.jsx
+++ Lead Management System/src/views/leadManagement/fileUpload/FileUpload.jsx
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import "./styles.css"
 
 function FileUpload() {
+    const [csvFile, setCsvFile] = useState(null);
+
     return (
         <div className="form-container">
             <h2 className="upload-title">Upload File</h2>
-            <form id="file-upload-form" className="uploader">
-                <input id="file-upload" type="file" name="fileUpload" accept=".xlsx, .xls, .csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel, text/plain"/>
+            <span id="file-upload-form" className="uploader">
+                <input id="file-upload" type="file" name="fileUpload"
+                       accept=".xlsx, .xls, .csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel, text/plain"
+                       onChange={(e) => {
+                           const file = e.target.files;
+                           console.log("files", file);
+                           if (!file.length) return;
+                           setCsvFile(file)
+
+                       }}
+                       onClick={(event) => {
+                           event.target.value = null
+                       }}/>
 
                 <label htmlFor="file-upload" id="file-drag">
-                    {/*<img id="file-image" src="#" alt="Preview" className="hidden"/>*/}
-                        <div id="start">
-                            <i className="fa fa-download" aria-hidden="true"></i>
-                            <div>Select a file or drag here</div>
-                            <div id="notimage" className="hidden">Please select an image</div>
-                            <span id="file-upload-btn" className="btn btn-primary">Select a file</span>
-                        </div>
-                        <div id="response" className="hidden">
-                            <div id="messages"></div>
-                            <progress className="progress" id="file-progress" value="0">
-                                <span>0</span>%
-                            </progress>
-                        </div>
+                    <div id="start">
+                        <i className="fa fa-download" aria-hidden="true"></i>
+                        <div>Select a file</div>
+                        <span id="file-upload-btn" type="bu" className="btn btn-primary">Select a file</span>
+                        {csvFile && <p>{csvFile[0]?.name}</p>}
+                    </div>
+                    <div id="response" className="hidden">
+                        <div id="messages"></div>
+                        <progress className="progress" id="file-progress" value="0">
+                            <span>0</span>%
+                        </progress>
+                    </div>
                 </label>
-            </form>
+
+            </span>
         </div>
     );
 }
--- Lead Management System/src/views/leadManagement/index.jsx
+++ Lead Management System/src/views/leadManagement/index.jsx
@@ -1,10 +1,63 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import FileUpload from "./fileUpload/FileUpload.jsx";
+import {Form} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
-function Index(props) {
+function Index() {
+    const [extraFields, setExtraFields] = React.useState([""]);
     return (
         <>
-            <FileUpload/>
+            <Form>
+                <FileUpload/>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Name Mapper</Form.Label>
+                    <Form.Control type="text" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formPhone">
+                    <Form.Label>Mobile Number Mapper</Form.Label>
+                    <Form.Control type="text"/>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formStatus">
+                    <Form.Label>Status</Form.Label>
+                    <Form.Select aria-label="Default select example">
+                        <option>...</option>
+                        <option value="1">Opening</option>
+                        <option value="2">Pending</option>
+                        <option value="3">Completed</option>
+                    </Form.Select>
+                </Form.Group>
+                <Form.Label>Extra Fields</Form.Label>
+
+                {
+                    extraFields.map((inputField, idx)=>(
+                        <Form.Group key={idx} className="mb-3" controlId={`formStatus`+idx}>
+                          <Form.Control type="text"/>
+
+                    </Form.Group>))
+                }
+                <Button variant="outline-secondary" onClick={()=>{
+                    setExtraFields(prevState => ([...prevState, ""]))
+                }} >
+                    Add Extra Fields
+                </Button>
+                {extraFields.length>0 &&<Button variant="outline-secondary" onClick={() => {
+                    setExtraFields(prevState => {
+                        const temp = [...prevState]
+                        temp.splice(temp.length - 1, 1)
+                        return temp
+                    })
+                }}>
+                    Delete
+                </Button>}
+
+                <div className="d-grid gap-2">
+                    <Button variant="primary" type="submit" size="lg">
+                        Submit
+                    </Button>
+                </div>
+            </Form>
         </>
     );
 }
