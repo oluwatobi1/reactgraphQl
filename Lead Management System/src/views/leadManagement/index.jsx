d0d43f2dfb56e5354a8c31178232b41aeaa9ec07
oluwatobi1 <akintunlesetobi@gmail.com>
2023-01-08 15:10:08 +0100

    feat:added file upload ui

---
+++ Lead Management System/src/views/leadManagement/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FileUpload from "./fileUpload/FileUpload.jsx";
+
+function Index(props) {
+    return (
+        <>
+            <FileUpload/>
+        </>
+    );
+}
+
+export default Index;
